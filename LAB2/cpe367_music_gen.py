cc87e6cb57bbd9c7935fea39ea0187d440626211
seasaltsaige <oriontothemax@gmail.com>
2026-01-25 20:00:35 -0800

    music gen wav file

--- LAB2/cpe367_music_gen.py
+++ LAB2/cpe367_music_gen.py
@@ -19,12 +19,9 @@ from cpe367_wav import cpe367_wav
 # define function to add one note to list
 # students - modify this function as needed!
 
-def add_note(xlist,amp,w0,nstart,nlen):
-
-	# this initial version of the function only includes a tone burst
-	#  no harmonics and no decaying envelope are included
-	for n in range(nstart,nstart+nlen):
-		xlist[n] += amp * math.sin(w0 * n)
+def add_note(xlist,amp,w0,nstart,nlen,sigma):
+	for n in range(nstart, nstart+nlen):
+		xlist[n] += math.exp(-(n - nstart) / (sigma)) * amp * math.sin(w0 * (n - nstart))
 
 	# note summed into signal
 	return
@@ -66,21 +63,109 @@ def gen_wav(fpath_wav_out):
 
 	# these parameters will need updating!
 	#  you may also wish to add more parameters
-	total_num_samples = 32000
+	total_num_samples = 41040
 	
 	# allocate list of zeros to store an empty signal
 	xlist = [0] * total_num_samples
 
 	# setup one note
 	#  this implementation does not include harmonics or a decay
-	w1 = 2 * math.pi * 440 / sample_rate_hz
-	amp = 10000
+	w = 2 * math.pi / sample_rate_hz
+	max_amp = 32767*0.75;
+
+	# Number of beats in the sample
+	num_beats = 9;
+	# Yields samples per beat
+	beat_dur_samples = total_num_samples / num_beats;
+
+	# Duration of treble and bass notes in samples
+	treble_len = (beat_dur_samples)
+	bass_len = (beat_dur_samples * 3)
+
+	# Sigma values for treble and bass
+	treble_sigma_base = treble_len * 0.5
+	bass_sigma_base = bass_len * 0.75
+
+	# Treble notes in sequence, first note is 0, as there is no note
+	treble_notes = [ 0, 392, 440, 493.88, 587.33, 523.25, 523.25, 659.25, 587.33 ]
+	# Bass notes in sequence
+	bass_notes = [ 98, 196, 164.81 ]
+	# Number of harmonics to generate for bass and treble
+	num_treble_harmonics = 20
+	num_bass_harmonics = 8
 	
-	n_start = 8000
-	n_durr = 4000
-	
-	add_note(xlist,amp,w1,n_start,n_durr)
-	
+	# Generate each treble note
+	for n_index in range(len(treble_notes)):
+		# Grab the current note
+		note = treble_notes[n_index]
+		
+		# Counter to keep track of harmonics used
+		count = 0
+		# Counter to keep track of how many high/low (amplitude) harmonics to generate in order
+		harm_count = 1
+
+		while count != num_treble_harmonics:
+			# Generate high (amplitude) harmonics
+			for __ in range(harm_count):
+				add_note(
+					xlist,
+					(max_amp/(2*(count+1))),
+					w * (count+1) * note,
+					round(n_index * treble_len),
+					round(treble_len),
+					treble_sigma_base
+				)
+				count += 1
+
+			# Generate low (amplitude) harmonics
+			for __ in range(harm_count):
+				add_note(
+					xlist,
+					(max_amp/(2*(count+1))) / 5,
+					w * (count+1) * note,
+					round(n_index * treble_len),
+					round(treble_len),
+					treble_sigma_base
+				)
+				count += 1
+
+			# Increase harmonic count
+			# Starts at 1: (1 high, 1 low)
+			# Next is at 2: (2 high, 2 low)
+			# Etc, etc. Mimics as close as possible (without hardcoding) the spectrogram of the real sample
+			harm_count += 1
+
+
+
+	# The same as above, expect for the bass notes now
+	# Only other major difference is that the harmonic count starts at 4
+	for n_index in range(len(bass_notes)):
+		note = bass_notes[n_index]
+		count = 0
+		harm_count = 4
+		while count != num_bass_harmonics:
+			for __ in range(harm_count):
+				add_note(
+					xlist,
+					(max_amp/(2*(count+1))),
+					w * (count) * note,
+					round(n_index * bass_len),
+					round(bass_len),
+					bass_sigma_base
+				)
+				count += 1
+			for __ in range(harm_count):
+				add_note(
+					xlist,
+					(max_amp/(2*(count+1))) / 5,
+					w * (count) * note,
+					round(n_index * bass_len),
+					round(bass_len),
+					bass_sigma_base 
+				)
+				count += 1
+
+			harm_count += 1
 	
 	# students - well done!
 	###############################################################
