c625e22c49771ee5b51a00d4af7233a1a2198057
seasaltsaige <oriontothemax@gmail.com>
2026-03-06 15:25:54 -0800

    part 1

--- LAB7/cpe367_delay.py
+++ LAB7/cpe367_delay.py
@@ -14,114 +14,127 @@ import time
 from cpe367_wav import cpe367_wav
 from my_fifo import my_fifo
 
-	
+
 def load_samples(N: int, samps: list):
-	return samps[0:N]
+    return samps[0:N]
 
 
 def calc_dft(samples: list):
-	N = len(samples)
-	n = np.arange(N)
-	k = n.reshape((N, 1))
-	e = np.exp(-2j * np.pi * k * n / N)
+    # Length of the sample list
+    LEN = len(samples)
+    # Row vector
+    n = np.arange(LEN)
+    # Column vector
+    k = n.reshape((LEN, 1))
+    # get the twiddles (matrix, n x k [or k x n i forget])
+    e = np.exp(-2j * np.pi * k * n / LEN)
+    # compute the dot product for each row to get output dft vector
+    DFT = np.dot(e, samples) / LEN
+    return DFT
 
-	X = np.dot(e, samples)
-	return X
 
-def plot_points(dft: list):
-	freqs = []
-	mags = []
-	for i in range(1,2000):
-		freqs.append(i)
-		mags.append(np.abs(dft[i])) 
+import matplotlib.pyplot as plt
+import numpy as np
 
-	fig,ax = plt.subplots()
-	ax.plot(freqs, mags)
 
-	ax.set(xlabel='Freq (Hz)', ylabel='Counts', title='Nice')
-	ax.grid()
-	# fig.savefig('image.png')
-	plt.show()
-	return
+def plot_spectrum(magnitude: list, fs: int, N: int):
+
+    freqs = []
+    mags = []
+
+    for k in range(1, N // 2):
+
+        freq = k * fs / N
+
+        if freq > 2000:
+            break
+
+        freqs.append(freq)
+        mags.append(np.abs(magnitude[k]))
+
+    fig, ax = plt.subplots()
+
+    ax.plot(np.array(freqs), np.array(mags))
+
+    ax.set(xlabel="Frequency (Hz)", ylabel="Counts", title="Magnitude Spectrum")
+
+    ax.grid()
+
+    plt.show()
+
 
 ############################################
 ############################################
 # define routine for implementing a digital filter
-def process_wav(fpath_wav_in,fpath_wav_out):
-	"""
-	: this example does not implement an echo!
-	: input and output is accomplished via WAV files
-	: return: True or False 
-	"""
-	
-	# construct objects for reading/writing WAV files
-	#  assign each object a name, to facilitate status and error reporting
-	wav_in = cpe367_wav('wav_in',fpath_wav_in)
-	# wav_out = cpe367_wav('wav_out',fpath_wav_out)
-	
-	# open wave input file
-	ostat = wav_in.open_wav_in()
-	if ostat == False:
-		print('Cant open wav file for reading')
-		return False
-		
-	# setup configuration for output WAV
-	num_channels = 1
-	sample_width_8_16_bits = 16
-	sample_rate_hz = 8000
-	# wav_out.set_wav_out_configuration(num_channels,sample_width_8_16_bits,sample_rate_hz)
+def process_wav(fpath_wav_in, fpath_wav_out):
+    """
+    : this example does not implement an echo!
+    : input and output is accomplished via WAV files
+    : return: True or False
+    """
 
-	# open WAV output file
-	# ostat = wav_out.open_wav_out()
-	# if ostat == False:
-	# 	print('Cant open wav file for writing')
-	# 	return False
-	
-	###############################################################
-	###############################################################
-	# students - allocate your fifo, with an appropriate length (M)
+    # construct objects for reading/writing WAV files
+    #  assign each object a name, to facilitate status and error reporting
+    wav_in = cpe367_wav("wav_in", fpath_wav_in)
+    # wav_out = cpe367_wav('wav_out',fpath_wav_out)
 
-	N = 4000
+    # open wave input file
+    ostat = wav_in.open_wav_in()
+    if ostat == False:
+        print("Cant open wav file for reading")
+        return False
 
-	samples = []
+    # setup configuration for output WAV
+    num_channels = 1
+    sample_width_8_16_bits = 16
+    sample_rate_hz = 8000
+    # wav_out.set_wav_out_configuration(num_channels,sample_width_8_16_bits,sample_rate_hz)
 
+    # open WAV output file
+    # ostat = wav_out.open_wav_out()
+    # if ostat == False:
+    # 	print('Cant open wav file for writing')
+    # 	return False
 
+    ###############################################################
+    ###############################################################
+    # students - allocate your fifo, with an appropriate length (M)
 
-	xin = 0
-	n = 0
-	while xin != None:
-	
-		xin = wav_in.read_wav()
-		if xin == None: break
-		
-		samples.append(xin)
-		
-		n += 1
-			
+    N = 4000
 
-	first = load_samples(N, samples)
+    samples = []
 
-	dft = calc_dft(first)
-	# print(len(dft))
-	plot_points(dft)
-	
+    xin = 0
+    n = 0
+    while xin != None:
 
-	# 	# convert to signed int
-	# 	yout = int(round(yout))
-		
-	# 	# output current sample
-	# 	ostat = wav_out.write_wav(yout)
-	# 	if ostat == False: break
-	
-	# # close input and output files
-	# #  important to close output file - header is updated (with proper file size)
-	wav_in.close_wav()
-	# wav_out.close_wav()
-		
-	return True
+        xin = wav_in.read_wav()
+        if xin == None:
+            break
 
+        samples.append(xin)
 
+        n += 1
 
+    first = load_samples(N, samples)
+
+    dft = calc_dft(first)
+    # print(len(dft))
+    plot_spectrum(dft, sample_rate_hz, len(dft))
+
+    # 	# convert to signed int
+    # 	yout = int(round(yout))
+
+    # 	# output current sample
+    # 	ostat = wav_out.write_wav(yout)
+    # 	if ostat == False: break
+
+    # # close input and output files
+    # #  important to close output file - header is updated (with proper file size)
+    wav_in.close_wav()
+    # wav_out.close_wav()
+
+    return True
 
 
 ############################################
@@ -129,61 +142,27 @@ def process_wav(fpath_wav_in,fpath_wav_out):
 # define main program
 def main():
 
-	# check python version!
-	major_version = int(sys.version[0])
-	if major_version < 3:
-		print('Sorry! must be run using python3.')
-		print('Current version: ')
-		print(sys.version)
-		return False
-	if len(sys.argv) < 3:
-			return print("err");
-	# grab file names
-	fpath_wav_in = sys.argv[1]
-	fpath_wav_out = sys.argv[2]
+    # check python version!
+    major_version = int(sys.version[0])
+    if major_version < 3:
+        print("Sorry! must be run using python3.")
+        print("Current version: ")
+        print(sys.version)
+        return False
+    if len(sys.argv) < 2:
+        return print("err")
 
-	if not fpath_wav_in:
-		return print("Err")
-	
-	
-	
-	############################################
-	############################################
-	# test signal history
-	#  feel free to comment this out, after verifying
-		
-	# allocate history
-	M = 3
-	fifo = my_fifo(M)
-	
+    # grab file names
+    fpath_wav_in = sys.argv[1]
+    # fpath_wav_out = sys.argv[2]
 
-	# add some values to history
-	fifo.update(1)
-	fifo.update(2)
-	fifo.update(3)
-	fifo.update(4)
-	
-	# print out history in order from most recent to oldest
-	print('signal history - test')
-	for k in range(M):
-		print('hist['+str(k)+']='+str(fifo.get(k)))
+    if not fpath_wav_in:
+        return print("Err")
 
-	############################################
-	############################################
-	
+    return process_wav(fpath_wav_in, "")
 
 
-	# let's do it!
-	return process_wav(fpath_wav_in,fpath_wav_out)
-	
-			
-	
-	
-	
-############################################
-############################################
-# call main function
-if __name__ == '__main__':
-	
-	main()
-	quit()
+if __name__ == "__main__":
+
+    main()
+    quit()
